2f721622c7aa2dbb5ef6700f2b25f7d309d1d132
Johnfmack <jmackay101@gmail.com>
2021-09-29 10:19:17 +1000

    edited app content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { Route, Switch }from 'react-router-dom'
-import AllMeetups from './pages/AllMeetups'
-import NewMeetup from './pages/NewMeetup'
-import Favorites from './pages/Favorites'
-import Layout from './components/layout/Layout'
+import { Route, Switch } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-   
     <Layout>
-     <Switch>
-     <Route path='/' exact><AllMeetups />  </Route>
-     <Route path='/new-meetup'><NewMeetup /></Route>
-     <Route path='/favorites'><Favorites /></Route>
-     </Switch>
-    <p>yo yo</p>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetups />{" "}
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
     </Layout>
-   
   );
 }
 
